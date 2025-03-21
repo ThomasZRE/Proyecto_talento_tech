3eca95693498b44a047c83a155e245d294719497
ThomasZRE <zapatat4@gmail.com>
2025-03-21 09:29:00 -0500

    Backend

---
+++ Backend/Visualizador/src/test/java/com/visualizador/Visualizador/VisualizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visualizador.Visualizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualizadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
